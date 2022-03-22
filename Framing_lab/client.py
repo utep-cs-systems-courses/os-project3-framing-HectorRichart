dc489652dfa17a4856623d1e33afde7c41e99087
hrrichartru <hrrichartru@CCS31008-167607.local>
2022-03-22 15:19:42 -0600

    Fixed code to receive file and handle multiple clientes, doing output of the different clients

--- Framing_lab/client.py
+++ Framing_lab/client.py
@@ -58,12 +58,41 @@ if delay != 0:
     time.sleep(int(delay))
     print("done sleeping")
 
+#clients receives and figures out file size, extension and where they end and start, output file will contain
+#all the files restored correclty
 while 1:
-    #data = s.recv(1024)
-    #print("Encoded data '%s'"% data)
     data = s.recv(1024).decode()
     print("Received '%s'" % data)
     if len(data) == 0:
         break
 print("Zero length read.  Closing")
 s.close()
+
+# import socket
+
+# #Gets a socket object
+# ClientSocket = socket.socket()
+# #create address parameters
+# host = '127.0.0.1'
+# port = 1233
+
+# #It tries to connect to the server
+# print('Waiting for connection')
+# try:
+#     ClientSocket.connect((host, port))
+# except socket.error as e:
+#     print(str(e))
+
+# #Opens the file a and receives a 1024 bytes from the server MAX
+# Response = ClientSocket.recv(1024)
+# #Create the file
+# clientid = str(ClientSocket.getsockname()[1])
+# text_file = open("received_file_[" + clientid +"].txt","a")
+# #Write the file as long as you are receiving it from server
+# while len(Response) > 0:
+#     Response = ClientSocket.recv(1024)
+#     text_file.write(Response.decode())
+#     print(Response.decode('utf-8'))
+    
+# #Close
+# ClientSocket.close()
--- Framing_lab/server.py
+++ Framing_lab/server.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
+from http import client
 import socket, sys, re, os, time
 sys.path.append("../lib")       # for params
 import params
@@ -22,7 +23,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((listenAddr, listenPort))
 s.listen(1)              # allow only one outstanding request
 # s is a factory for connected sockets
-
+#Fork on every call to get a child that 
 while True:
     conn, addr = s.accept() # wait until incoming connection request (and accept it)
     if os.fork() == 0:      # child becomes server
@@ -37,6 +38,49 @@ while True:
                 if sent == 0:
                     raise RuntimeError("Socket connection broken.")
                 totalsent += sent
+                unarchive("received file from client" + str(addr[1]) + ".txt", encoded_file)
             time.sleep(0.25);       # delay 1/4s
             conn.shutdown(socket.SHUT_WR)
             sys.exit(0)
+
+
+# import socket
+# from _thread import *
+
+# #Creates the socket
+# ServerSocket = socket.socket()
+# #Create the addresses
+# host = '127.0.0.1'
+# port = 1233
+# #set to listen from income connections
+# ThreadCount = 0
+# try:
+#     ServerSocket.bind((host, port))
+# except socket.error as e:
+#     print(str(e))
+
+# print('Waiting for a Connection..')
+# ServerSocket.listen(5)
+
+# #Function to send the info to the client
+# def threaded_client(connection):
+#     #open the file
+#     bio_text = open("sockets.txt")
+#     connection.send(str.encode('Welcome to the Server'))
+#     #read the file
+#     buffer = bio_text.read()
+#     #Sends 1024 bytes at the time to the client 
+#     while len(buffer) > 0:
+#         connection.send(buffer[:1024].encode())
+#         buffer = buffer[1024:]
+#     #close the conncetion
+#     connection.close()
+
+# #Creates new threads for every connection
+# while True:
+#     Client, address = ServerSocket.accept()
+#     print('Connected to: ' + address[0] + ':' + str(address[1]))
+#     start_new_thread(threaded_client, (Client, ))
+#     ThreadCount += 1
+#     print('Thread Number: ' + str(ThreadCount))
+# ServerSocket.close()

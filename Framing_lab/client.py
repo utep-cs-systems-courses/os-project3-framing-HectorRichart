b0e6f9f1213de0c8fe06dec7e8e0c7c72fb682dc
hrrichartru <hrrichartru@CCS31008-167607.local>
2022-03-22 15:27:54 -0600

    cleaned code

--- Framing_lab/client.py
+++ Framing_lab/client.py
@@ -67,32 +67,3 @@ while 1:
         break
 print("Zero length read.  Closing")
 s.close()
-
-# import socket
-
-# #Gets a socket object
-# ClientSocket = socket.socket()
-# #create address parameters
-# host = '127.0.0.1'
-# port = 1233
-
-# #It tries to connect to the server
-# print('Waiting for connection')
-# try:
-#     ClientSocket.connect((host, port))
-# except socket.error as e:
-#     print(str(e))
-
-# #Opens the file a and receives a 1024 bytes from the server MAX
-# Response = ClientSocket.recv(1024)
-# #Create the file
-# clientid = str(ClientSocket.getsockname()[1])
-# text_file = open("received_file_[" + clientid +"].txt","a")
-# #Write the file as long as you are receiving it from server
-# while len(Response) > 0:
-#     Response = ClientSocket.recv(1024)
-#     text_file.write(Response.decode())
-#     print(Response.decode('utf-8'))
-    
-# #Close
-# ClientSocket.close()
